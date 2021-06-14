43ea627cdce561bfe370305c5861b08beea3d3e7
rifanid98 <rifanid98@users.noreply.github.com>
2021-06-14 05:08:10 +0000

    deploy: 8da431befe8787fdda4929ee27b2bc6c6a027b92

---
+++ precache-manifest.3ab53f8ef4f5146cc85dd9c69f1cf9ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed742610cbe0f62eec9dfee061b8424b",
+    "url": "https://rifanid98.github.io/task-management-frontend-react/index.html"
+  },
+  {
+    "revision": "42ae57c23fcbc2398eea",
+    "url": "https://rifanid98.github.io/task-management-frontend-react/static/css/main.7f14e98f.chunk.css"
+  },
+  {
+    "revision": "c3906ba6fb58e3dc62e8",
+    "url": "https://rifanid98.github.io/task-management-frontend-react/static/js/2.65794664.chunk.js"
+  },
+  {
+    "revision": "42ae57c23fcbc2398eea",
+    "url": "https://rifanid98.github.io/task-management-frontend-react/static/js/main.4ac929bd.chunk.js"
+  },
+  {
+    "revision": "5aff7df3684aef03643a",
+    "url": "https://rifanid98.github.io/task-management-frontend-react/static/js/runtime~main.eae1077b.js"
+  }
+]);
